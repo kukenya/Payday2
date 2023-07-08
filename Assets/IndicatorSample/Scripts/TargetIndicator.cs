d906747da458c0c387008c9fc8dc795d0a9b5237
Kukenya <jbs7917@gmail.com>
2023-07-08 11:00:50 +0900

    20230708 11:00

--- Assets/IndicatorSample/Scripts/TargetIndicator.cs
+++ Assets/IndicatorSample/Scripts/TargetIndicator.cs
@@ -7,20 +7,21 @@ public class TargetIndicator : MonoBehaviour
 {
     public Image TargetIndicatorImage;
     public Image OffScreenTargetIndicator;
-    public Image OffScreenTargetIndicatorImage;
     public float outOfSightOffset = 20f;
+    public float imageOffSet = 10f;
+
+    public float targetImageMoveTimeFront = 100f;
+    public float targetImageMoveTimeOut = 0.1f;
+    private float targetImageMoveTime;
 
     private GameObject target;
     private Camera mainCamera;
     private RectTransform canvasRect;
 
-    private RectTransform rectTransform;
-
-    private void Awake()
+    private void Start()
     {
-        rectTransform = GetComponent<RectTransform>();
+        targetImageMoveTime = targetImageMoveTimeFront;
     }
-
 
     public void InitialiseTargetIndicator(GameObject target, Camera mainCamera, Canvas canvas)
     {
@@ -39,47 +40,55 @@ public class TargetIndicator : MonoBehaviour
     {
         //타겟 위치를 캔버스 좌표로 변환
         Vector3 indicatorPosition = mainCamera.WorldToScreenPoint(target.transform.position);
-        Vector3[] indicatorPositions = null;
-        Debug.Log(indicatorPosition);
+        Vector3 indicator2Position = indicatorPosition;
 
         //카메라 앞에서 보여질때
         if (indicatorPosition.z >= 0f & indicatorPosition.x <= canvasRect.rect.width * canvasRect.localScale.x
          & indicatorPosition.y <= canvasRect.rect.height * canvasRect.localScale.x & indicatorPosition.x >= 0f & indicatorPosition.y >= 0f)
         {
-
+            if(TargetIndicatorImage.rectTransform.position.x == indicatorPosition.x &)
+            {
+                targetImageMoveTime = targetImageMoveTimeFront;
+            }
+            
             //바꿔도 안바꿔도 노상관 
             indicatorPosition.z = 0f;
 
             //Target is in sight, change indicator parts around accordingly
-            targetOutOfSight(false, indicatorPosition, new Vector3(0, 0));
+            targetOutOfSight(false, indicatorPosition);
         }
 
         //내 앞에 있지만 시야 밖일때
         else if (indicatorPosition.z >= 0f)
         {
+            targetImageMoveTime = targetImageMoveTimeOut;
             //Set indicatorposition and set targetIndicator to outOfSight form.
-            indicatorPositions = OutOfRangeindicatorPositionB(indicatorPosition);
-            targetOutOfSight(true, indicatorPositions[0], indicatorPositions[1]);
+            indicatorPosition = OutOfRangeindicatorPositionB(indicatorPosition);
+            indicator2Position = OutOfRangeindicator2(indicatorPosition);
+            //indicator2Position = -(indicatorPosition - OutOfRangeindicator2(indicatorPosition));
+            targetOutOfSight(true, indicatorPosition);
         }
         //내 앞도 아니고 시야 밖일때
         else
         {
+            targetImageMoveTime = targetImageMoveTimeOut;
             //Invert indicatorPosition! Otherwise the indicator's positioning will invert if the target is on the backside of the camera!
             indicatorPosition *= -1f;
 
             //Set indicatorposition and set targetIndicator to outOfSight form.
-            indicatorPositions = OutOfRangeindicatorPositionB(indicatorPosition);
-            targetOutOfSight(true, indicatorPositions[0], indicatorPositions[1]);
+            indicatorPosition = OutOfRangeindicatorPositionB(indicatorPosition);
+            indicator2Position = OutOfRangeindicator2(indicatorPosition);
+            //indicator2Position = -(indicatorPosition - OutOfRangeindicator2(indicatorPosition));
+            targetOutOfSight(true, indicatorPosition);
 
         }
 
         //Set the position of the indicator
-        rectTransform.position = indicatorPositions[0];
-        //OffScreenTargetIndicatorImage.rectTransform.position = indicatorPositions[1];
-
+        TargetIndicatorImage.rectTransform.position = Vector3.Lerp(TargetIndicatorImage.rectTransform.position, indicatorPosition, targetImageMoveTime);
+        OffScreenTargetIndicator.rectTransform.position = indicator2Position;
     }
 
-    private Vector3[] OutOfRangeindicatorPositionB(Vector3 indicatorPosition)
+    private Vector3 OutOfRangeindicatorPositionB(Vector3 indicatorPosition)
     {
         //Set indicatorPosition.z to 0f; We don't need that and it'll actually cause issues if it's outside the camera range (which easily happens in my case)
         indicatorPosition.z = 0f;
@@ -92,29 +101,40 @@ public class TargetIndicator : MonoBehaviour
         indicatorPosition.x = Mathf.Cos(Mathf.Deg2Rad * angle) * outOfSightOffset;
         indicatorPosition.y = Mathf.Sin(Mathf.Deg2Rad * angle) * outOfSightOffset;
 
-        Vector3 indicatorPosition2 = new Vector3(Mathf.Cos(Mathf.Deg2Rad * angle) * outOfSightOffset - 20f, Mathf.Sin(Mathf.Deg2Rad * angle) * outOfSightOffset - 20f);
-
         indicatorPosition += canvasCenter;
-        indicatorPosition2 += canvasCenter;
 
-        Vector3[] indicatorPositions = new Vector3[2];
-        indicatorPositions[0] = indicatorPosition;
-        indicatorPositions[1] = indicatorPosition2;
+        return indicatorPosition;
+    }
 
-        return indicatorPositions;
+    private Vector3 OutOfRangeindicator2(Vector3 indicator2Position)
+    {
+
+        indicator2Position.z = 0f;
+
+        //Calculate Center of Canvas and subtract from the indicator position to have indicatorCoordinates from the Canvas Center instead the bottom left!
+        Vector3 canvasCenter = new Vector3(canvasRect.rect.width / 2f, canvasRect.rect.height / 2f, 0f) * canvasRect.localScale.x;
+        indicator2Position -= canvasCenter;
+
+        float angle = Vector3.SignedAngle(Vector3.right, indicator2Position, Vector3.forward);
+        indicator2Position.x = Mathf.Cos(Mathf.Deg2Rad * angle) * (outOfSightOffset + imageOffSet);
+        indicator2Position.y = Mathf.Sin(Mathf.Deg2Rad * angle) * (outOfSightOffset + imageOffSet);
+
+        indicator2Position += canvasCenter;
+
+        return indicator2Position;
     }
 
 
 
-    private void targetOutOfSight(bool oos, Vector3 indicatorPosition, Vector3 indicatorPosition2)
+    private void targetOutOfSight(bool oos, Vector3 indicatorPosition)
     {
         //In Case the indicator is OutOfSight
         if (oos)
         {
             //Activate and Deactivate some stuff
             if (OffScreenTargetIndicator.gameObject.activeSelf == false) OffScreenTargetIndicator.gameObject.SetActive(true);
-            if (OffScreenTargetIndicatorImage.gameObject.activeSelf == false) OffScreenTargetIndicatorImage.gameObject.SetActive(true);
-            if (TargetIndicatorImage.isActiveAndEnabled == true) TargetIndicatorImage.enabled = false;
+            if (TargetIndicatorImage.gameObject.activeSelf == false) TargetIndicatorImage.gameObject.SetActive(true);
+            //if (TargetIndicatorImage.isActiveAndEnabled == true) TargetIndicatorImage.enabled = false;
 
             //Set the rotation of the OutOfSight direction indicator
             OffScreenTargetIndicator.rectTransform.rotation = Quaternion.Euler(rotationOutOfSightTargetindicator(indicatorPosition));
@@ -131,8 +151,8 @@ public class TargetIndicator : MonoBehaviour
         else
         {
             if (OffScreenTargetIndicator.gameObject.activeSelf == true) OffScreenTargetIndicator.gameObject.SetActive(false);
-            if (OffScreenTargetIndicatorImage.gameObject.activeSelf == true) OffScreenTargetIndicatorImage.gameObject.SetActive(false);
-            if (TargetIndicatorImage.isActiveAndEnabled == false) TargetIndicatorImage.enabled = true;
+            //if (TargetIndicatorImage.gameObject.activeSelf == true) TargetIndicatorImage.gameObject.SetActive(false);
+            //if (TargetIndicatorImage.isActiveAndEnabled == false) TargetIndicatorImage.enabled = true;
         }
     }
 
